8ce17c2662b9aebf9ac7bf87d6c38b3d94b7007d
TowfMinh <vinxen2019@gmail.com>
2023-10-24 15:06:37 +0700

    first

---
+++ app/src/main/java/com/mad/g1/nguyentuanminh/blogapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.mad.g1.nguyentuanminh.blogapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
