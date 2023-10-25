edcc115df314b609237dbfb8059cd1ae63904a3a
TowfMinh <vinxen2019@gmail.com>
2023-10-25 15:31:00 +0700

    cap nhat

--- app/src/main/java/com/mad/g1/nguyentuanminh/blogapp/model/User.java
+++ app/src/main/java/com/mad/g1/nguyentuanminh/blogapp/model/User.java
@@ -4,21 +4,23 @@ import java.util.Date;
 
 public class User {
     int id;
-    String username;
+    String email;
     String password;
     String role;
     String fullname;
     Date dob;
     String pob;
+    String gender;
 
-    public User(int id, String username, String password, String role, String fullname, Date dob, String pob) {
+    public User(int id, String email, String password, String role, String fullname, Date dob, String pob, String gender) {
         this.id = id;
-        this.username = username;
+        this.email = email;
         this.password = password;
         this.role = role;
         this.fullname = fullname;
         this.dob = dob;
         this.pob = pob;
+        this.gender = gender;
     }
 
     public int getId() {
@@ -29,12 +31,12 @@ public class User {
         this.id = id;
     }
 
-    public String getUsername() {
-        return username;
+    public String getEmail() {
+        return email;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getPassword() {
@@ -75,5 +77,13 @@ public class User {
 
     public void setPob(String pob) {
         this.pob = pob;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
     }
 }
--- app/src/main/java/com/mad/g1/nguyentuanminh/blogapp/view/LoginView.java
+++ app/src/main/java/com/mad/g1/nguyentuanminh/blogapp/view/LoginView.java
@@ -2,16 +2,29 @@ package com.mad.g1.nguyentuanminh.blogapp.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.mad.g1.nguyentuanminh.blogapp.R;
 
 public class LoginView extends AppCompatActivity {
+    TextView registerTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        registerTV = findViewById(R.id.registerText);
+
+        registerTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginView.this,RegisterView.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
